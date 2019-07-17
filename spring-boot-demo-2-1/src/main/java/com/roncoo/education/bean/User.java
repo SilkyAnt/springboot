4a9db376f573d8e8f30b544e6419ad34b03deba7
Silky_Ant <jianglp1965@gmail.com>
2019-07-17 11:31:12 +0800

    Spring Boot基础教程2-RESTfull API简单项目的快速搭建

---
+++ spring-boot-demo-2-1/src/main/java/com/roncoo/education/bean/User.java
@@ -0,0 +1,17 @@
+package com.roncoo.education.bean;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 实体类
+ * 
+ * @author Silky_Ant
+ */
+@Data
+public class User {
+	private int id;
+	private String name;
+	private Date date;
+}
